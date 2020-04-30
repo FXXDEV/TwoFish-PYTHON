3cbfb7bcbfc4949cb0cee016441dbca887b3fe08
FXXDEV <fxxdev@outlook.com>
2020-04-30 00:29:10 -0300

    README.md changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TwoFish-PYTHON
+
+#####  ECB/ 128bits - hex
+
+#### Steps to run
+1. Run python3 /chilkatFiles/installChilkat.py
+1. Run python3 twofish.py
--- twofish.py
+++ twofish.py
@@ -15,22 +15,20 @@ crypt.put_CipherMode("ecb")
 # KeyLength [128, 192, 256]
 crypt.put_KeyLength(128)
 
+#padding
 crypt.put_PaddingScheme(0)
 
+#encoding Mode
 crypt.put_EncodingMode("hex")
-
 
 keyHex = "6E3272357538782F413F4428472B4B62"
 crypt.SetEncodedKey(keyHex,"hex")
-# Encrypt a string...
-# The input string is 44 ANSI characters (i.e. 44 bytes), so
-# the output should be 48 bytes (a multiple of 16).
-# Because the output is a hex string, it should
-# be 96 characters long (2 chars per byte).
-str = input('Encrypt the string: ')
 
+#input string
+str = input('Encrypt the string: ')
 encStr = crypt.encryptStringENC(str)
 
+#prints sections 
 print('Encripty input string')
 print('-'*30)
 print(f'String: {str} \nCript:{encStr}')
@@ -41,7 +39,7 @@ group = [
     '0000000000000000', 
     '0000000000000001', 
     '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',#128block
-    '0x00000000000000000000000000000000', #128block
+    '0x00000000000000000000000000000000',#128block
     '0x00000000000000000000000000000001'#128block 
 ]
 for x in range(len(group)):
