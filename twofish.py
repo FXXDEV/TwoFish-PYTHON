00b9f297bdfc50145e4f1505c39ebbbdcd8f700a
FXXDEV <fxxdev@outlook.com>
2020-04-29 23:41:23 -0300

    Update static string + decrypt

--- twofish.py
+++ twofish.py
@@ -1,3 +1,4 @@
+#encoding: utf-8
 import chilkat
 import sys
 
@@ -6,38 +7,47 @@ glob = chilkat.CkGlobal()
 glob.UnlockBundle("Anything for 30-day trial")
 
 crypt = chilkat.CkCrypt2()
-
 crypt.put_CryptAlgorithm("twofish")
+
 # "ecb" / "cbc"
 crypt.put_CipherMode("ecb")
 
 # KeyLength [128, 192, 256]
 crypt.put_KeyLength(128)
 
-# Twofish has a block size of 16 bytes, so encrypted output is always
-# a multiple of 16.
 crypt.put_PaddingScheme(0)
 
-# EncodingMode specifies the encoding of the output for
-# encryption, and the input for decryption.
-# It may be "hex", "url", "base64", or "quoted-printable".
 crypt.put_EncodingMode("hex")
-# The secret key must equal the size of the key.  For
-# 256-bit encryption, the binary secret key is 32 bytes.
-# For 128-bit encryption, the binary secret key is 16 bytes.
-keyHex = "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"
-crypt.SetEncodedKey(keyHex,"hex")
 
+
+keyHex = "6E3272357538782F413F4428472B4B62"
+crypt.SetEncodedKey(keyHex,"hex")
 # Encrypt a string...
 # The input string is 44 ANSI characters (i.e. 44 bytes), so
 # the output should be 48 bytes (a multiple of 16).
 # Because the output is a hex string, it should
 # be 96 characters long (2 chars per byte).
 str = input('Encrypt the string: ')
-#encStr = crypt.encryptStringENC("The quick brown fox jumps over the lazy dog.")
-encStr = crypt.encryptStringENC(str)
-print(encStr)
 
-# Now decrypt:
-decStr = crypt.decryptStringENC(encStr)
-print(decStr)
+encStr = crypt.encryptStringENC(str)
+
+print('Encripty input string')
+print('-'*30)
+print(f'String: {str} \nCript:{encStr}')
+print('-'*30)
+print('\nEncripty static strs(ECB/128)\n\n')
+group = [
+    '1111111111111111',
+    '0000000000000000', 
+    '0000000000000001', 
+    '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',#128block
+    '0x00000000000000000000000000000000', #128block
+    '0x00000000000000000000000000000001'#128block 
+]
+for x in range(len(group)):
+  encStrL = crypt.encryptStringENC(group[x])
+  print(f'String:{group[x]}\nEncrypted:{encStrL}\nDecrypted:{crypt.decryptStringENC(encStrL)}')
+  print('-'*30) 
+  print('\n') 
+
+
